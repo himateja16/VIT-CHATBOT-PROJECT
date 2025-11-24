a374c1a810bf78cbe56de1a335b95b75288db716
himateja16 <menda.24mim10024@vitbhopal.ac.in>
2025-11-24 23:29:07 +0530

    Create FAQItem.java

---
+++ FAQItem.java
@@ -0,0 +1,10 @@
+public class FAQItem {
+
+    String question;
+    String answer;
+
+    public FAQItem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
